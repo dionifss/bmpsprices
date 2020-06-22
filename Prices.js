b402b3f8e6524d948c6e7da98918c17c4dc178fe
dionifss <59808260+dionifss@users.noreply.github.com>
2020-06-22 10:31:13 +0200

    Add files via upload

---
+++ Prices.js
@@ -0,0 +1,50 @@
+let recomended = document.getElementById("recomended").value;
+function boom() {
+  console.log("banana");
+
+  let recomended = document.getElementById("recomended").value;
+  let discount = document.getElementById("discount").value;
+  sellprice(recomended, discount);
+}
+
+function sellprice(recomended, discount) {
+  discount = discount % 100;
+  let normal;
+  let lownormal;
+  let lastsell;
+  let ultima;
+  let cuesta = Math.floor((recomended / 2) * (1 - discount) * 100) / 100;
+  if (recomended > 50) {
+    normal = Math.min(recomended * 0.9, recomended * (1 - discount + 0.2));
+    lownormal = Math.floor(recomended * (1 - discount - 0.08) * 10) / 10;
+    lastsell = Math.floor(recomended * (1 - discount - 0.18) * 10) / 10;
+    ultima = Math.floor(recomended * (1 - discount - 0.25) * 10) / 10;
+  } else if (recomended > 15) {
+    normal =
+      Math.floor(
+        Math.min(recomended * 0.95, recomended * (1 - discount + 0.2)) * 100
+      ) / 100;
+    lownormal = Math.floor(recomended * (1 - discount - 0.07) * 10) / 10;
+    lastsell = Math.floor(recomended * (1 - discount - 0.16) * 10) / 10;
+    ultima = Math.floor(recomended * (1 - discount - 0.23) * 10) / 10;
+  } else {
+    normal =
+      Math.floor(
+        Math.min(recomended, recomended * (1 - discount + 0.2)) * 100
+      ) / 100;
+    lownormal = Math.floor(recomended * (1 - discount) * 100) / 100;
+    lastsell = Math.floor(recomended * (1 - discount - 0.05) * 100) / 100;
+    ultima = Math.floor(recomended * (1 - discount - 0.1) * 100) / 100;
+  }
+  document.getElementById(
+    "answer"
+  ).innerHTML = `PVPR ${recomended}€ - Descuento ${
+    discount * 100
+  }% - Nos cuesta ${cuesta} - con IVA ${
+    Math.floor(cuesta * 1.21 * 100) / 100
+  }.  
+De salida a ${normal}, 
+Bajar a ${lownormal}, 
+Ultra rebaja ${lastsell} 
+Limpiar ultima talla ${ultima}`;
+}
